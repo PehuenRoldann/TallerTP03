ef464890738b49cca5c68d2c3621a3d4556fe380
pehuenroldan <pehuenroldan03@gmail.com>
2022-10-07 08:58:06 -0300

    Solucion 2 borrada

--- Ejercicio02_Figuras/FiguraGeometrica.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TP3Ejercicio02
-{
-    public abstract class FiguraGeometrica
-    {
-        public abstract double CalcularArea();
-        public abstract double CalcularPerimetro();
-    }
-}
